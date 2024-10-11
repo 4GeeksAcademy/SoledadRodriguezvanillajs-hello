e3bc913eaa425b29870fe715a6676512d8ebd061
soledadrodriguez <68907554+soledadrodriguez@users.noreply.github.com>
2024-10-11 23:19:46 +0000

    [First commit]

--- src/app.js
+++ src/app.js
@@ -7,5 +7,27 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+
+  function generarExcusas() {
+    let who = ["The dog", "My grandma", "The mailman", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "my phone", "the car"];
+    let when = [
+      "before the class",
+      "when I was sleeping",
+      "while I was exercising",
+      "during my lunch",
+      "while I was praying"
+    ];
+
+    let numwho = Math.floor(Math.random() * who.length);
+
+    let randomWho = [who.find(numwho)];
+  }
+  console.log(generarExcusas());
+
+  let paragraph = document.getElementById("excuse");
+  paragraph.innerText = "randomText";
+
   console.log("Hello Rigo from the console!");
 };
